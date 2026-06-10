f0b177c51da78a2ada86df130c3a09d01713b609
3LPR1M0 <elliotmayateran@gmail.com>
2026-06-09 20:15:13 -0400

    Create options.lua

---
+++ options.lua
@@ -0,0 +1,12 @@
+vim.opt.number = true
+
+vim.opt.scrolloff = 8
+
+-- make cursor like default vim
+vim.opt.guicursor = ""
+
+vim.opt.cursorline = true
+
+vim.opt.relativenumber = true
+
+vim.opt.shiftwidth= 4
