91feb36ae68549276e07547821f666dd2a9d638e
3LPR1M0 <elliotmayateran@gmail.com>
2026-09-10 19:13:20 -0400

    Update binds.lua

--- binds.lua
+++ binds.lua
@@ -1,2 +1,6 @@
 vim.g.mapleader = " "
-vim.keymap.set ("n", "<leader>cd", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>c1", function() vim.cmd.colorscheme("gruvbox") end, { desc = "Theme: gruvbox" })
+vim.keymap.set("n", "<leader>c2", function() vim.cmd.colorscheme("ayu-mirage") end, { desc = "Theme: ayu mirage" })
+vim.keymap.set("n", "<leader>c3", function() vim.cmd.colorscheme("cyberdream") end, { desc = "Theme: cyberdream" })
+vim.keymap.set("n", "<leader>c4", function() vim.cmd.colorscheme("gruber-darker") end, { desc = "Theme: gruber" })
